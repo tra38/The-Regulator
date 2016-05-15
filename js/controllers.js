c916b5ea9b72f9cc67f9d55f98df663c009b0716
tra38 <tra38@nau.edu>
2016-05-14 20:34:42 -0500

    Save and load data about corporations using LocalStorageService

--- js/controllers.js
+++ js/controllers.js
@@ -1,10 +1,27 @@
-app.controller('myCtrl', function($scope, corporations) {
+app.controller('myCtrl', ['$scope', 'corporations', 'localStorageService', function($scope, corporations, localStorageService) {
   $scope.apps = corporations.information();
-  $scope.selectedFactions = shuffle($scope.apps).slice(0,3)
+  $scope.selectedFactions = function()
+  {
+    var data = localStorageService.get("corporations")
+    if (data == undefined ) {
+      var data = shuffle($scope.apps).slice(0,3)
+      localStorageService.set("corporations", data)
+      return data
+    }
+    else
+    {
+      return data
+    }
+  }();
+
   $scope.growthFaction = prepareInitialStats($scope.selectedFactions[0], 1)
   $scope.valueFaction = prepareInitialStats($scope.selectedFactions[1], 2)
   $scope.stableFaction = prepareInitialStats($scope.selectedFactions[2], 3)
-});
+
+  $scope.attackTarget = function(faction) {
+    console.log("You pressed a button!")
+  }
+}]);
 
 app.controller('CorporateController', ['$scope', '$routeParams', 'corporations', function($scope, $routeParams, corporations) {
   $scope.faction = corporations.information()[$routeParams.id - 1];
@@ -15,5 +32,8 @@ app.controller('PlayerController', ['$scope', 'localStorageService', function($s
   $scope.changeName = function(name) {
     localStorageService.set("name", name);
     $scope.name = name;
+  }
+  $scope.attackTarget = function(faction) {
+    console.log("You pressed a button!")
   }
 }]);
